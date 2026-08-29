13588db2ad5423d2e5a920f63a5a57b2fc0692c8
samphors-roeung <your-email@example.com>
2026-08-29 14:43:34 +0700

    chore: add deploy.ps1 automated deployment script

---
+++ deploy.ps1
@@ -0,0 +1,5 @@
+﻿Write-Host '🚀 Updating and Deploying to https://sps-takeo.com...' -ForegroundColor Cyan
+git add .
+git commit -m " Auto deploy: 2026-08-29 14:43:25 \
+git push origin main
+Write-Host '✅ Website updated successfully!' -ForegroundColor Green
