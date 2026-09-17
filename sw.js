a9dcfc28f08be3ead9a55c38bbec7ed248bd70ea
samphors-roeung <your-email@example.com>
2026-09-17 08:19:08 +0700

    feat: use native HTML link for NTC Group E-Library and bust service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Sovannaphumi School 25, Takeo Campus - Network-First Real-time Master Service Worker
-const CACHE_NAME = 'sps-takeo-v6.2-master';
+const CACHE_NAME = 'sps-takeo-v6.7-master';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
