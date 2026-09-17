b3686e25d27a4dc277a148b11fc774b2eb45b468
samphors-roeung <your-email@example.com>
2026-09-17 09:30:34 +0700

    style: center bottom 2 Grade A cards under top 3 cards in Hall of Fame

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Sovannaphumi School 25, Takeo Campus - Network-First Real-time Master Service Worker
-const CACHE_NAME = 'sps-takeo-v7.0-master';
+const CACHE_NAME = 'sps-takeo-v7.1-master';
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
